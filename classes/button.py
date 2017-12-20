8621b28435ee64fbd60a2cabfa726d252393a27a
iPingouin <32131733+PingouinsECL@users.noreply.github.com>
2017-12-20 16:38:07 +0100

    Add files via upload

--- classes/button.py
+++ classes/button.py
@@ -1,8 +1,10 @@
+from math import sqrt
 class Button:
 
-    def __init__(self, picture, picture_hover, pos):
+    def __init__(self, picture, picture_hover, pos,type):
         self.x = pos[0]
         self.y = pos[1]
+        self.type = type          # type = 0 si la case est carré, type = 1 si la case hexagonale
         self.background = picture
         self.background_hover = picture_hover
 
@@ -13,6 +15,16 @@ class Button:
             win.blit(self.background, [self.x, self.y])
 
     def hover(self, cur):
-        # when hovered
         x_cur, y_cur = cur
-        return 0 < x_cur - self.x < self.background.get_width() and 0 < y_cur - self.y < self.background.get_height()
+        a = self.background.get_width()
+        xc, yc = self.x+a/2, self.y+a/2
+        h=sqrt(2)*a/(2+sqrt(2)) 
+        # when hovered
+        if self.type == 1:
+            if xc-a/2<= x_cur <= xc+a/2  and yc-a/2 <= y_cur <= yc+a/2 and y_cur >= -x_cur + yc +xc - (a+h)/2 and y_cur >= x_cur + yc - xc - (a+h)/2 and y_cur <= -x_cur + yc +xc + (a+h)/2 and y_cur <= x_cur + yc -xc +(a+h)/2:
+                print('non')
+            else:
+                print("Vous n'êtes pas sur la case") 
+                
+        else:
+            return 0 < x_cur - self.x < self.background.get_width() and 0 < y_cur - self.y < self.background.get_height()
