0dc9015821e460be975cd9fab0fe679c2ba29ccc
iPingouin <32131733+PingouinsECL@users.noreply.github.com>
2018-03-21 17:27:19 +0100

    Choice mode change

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ from board import *
 from player import *
 
 from select_mode import *
-from max_island import *
 
 from init_position import *
 from display_scores import *
@@ -197,6 +196,7 @@ while hold:
         
         # displaying background and buttons
         window.blit(background, pos_background)
+        window.blit(logo, pos_logo)
         but_choice0.show(window, pos_choice0)
         but_choice1.show(window, pos_choice1)     
         but_choice2.show(window, pos_choice2)
@@ -286,30 +286,14 @@ while hold:
         # computing islands and removing unaccessible cases
         board.compute_islands()
         for island in board.islands:
-            occupiers, island_cases, score = island
+            occupiers, island_cases = island
             if occupiers == []:
                 for (x, y) in island_cases:
                     board.cases_tab[y][x].change_state(0)
-            if len(occupiers) == 1 and len(island_cases) != 1:
-                pawns_on_island = []
-                calc = False
-                for i in range(len(players[occupiers[0]].pawns)):
-                    pawn=players[occupiers[0]].pawns[i]
-                    if (pawn.x,pawn.y) in island_cases:
-                        if pawn.isolate == False:
-                            pawn.isolate = True
-                            pawns_on_island.append(i)
-                        else:
-                            calc = True
-                            break
-                if not calc:
-                    m_island = max_island(board,players,occupiers[0], pawns_on_island, score)
-                    for i in pawns_on_island:
-                        players[occupiers[0]].pawns[i].remaining_actions = m_island
 
         # selecting the move if pawns left
         if players_lost[player_number] != 1:
-            fail, pawns, (direction, dist, pawn_number) = select_mode(board, players, table_button, player_number, window, background, pos_background)
+            fail, pawns, (direction, dist, pawn_number) = select_mode(board, players, table_button, player_number)
 
             # if a move was found
             if not(fail):
